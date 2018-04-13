3a279424a904acadd1c5dff33fcf6db4ca03f17c
chajs226 <chajs226@gmail.com>
2018-04-13 19:50:23 +0900

    Tank
    Direction 클래스 내부구현은 너무 어렵다. 일단 퉁치고넘어가고 Tank 클래스 구현 중, Tank 의 내부클래스인 Dialog 확인중

--- src/cjs/tankwar/component/Direction.java
+++ src/cjs/tankwar/component/Direction.java
@@ -1,9 +1,14 @@
 package cjs.tankwar.component;
 
 import java.io.Serializable;
+import java.util.Random;
+
+import cjs.tankwar.component.Direction;
 
 
-
+/* enum의 구현.. 1은 UP, 3은 UP_LEFT...
+ * 
+ */
 public enum Direction implements Serializable{
     UP(1),
     UP_LEFT(3),
@@ -17,5 +22,94 @@ public enum Direction implements Serializable{
 	
 	private static final Direction DIRECTIONS[] =
         { UP, UP_LEFT, LEFT, DOWN_LEFT, DOWN, DOWN_RIGHT, RIGHT, UP_RIGHT };
+    private static final Direction DIRECTIONS_WITH_STOP[] =
+        { UP, UP_LEFT, LEFT, DOWN_LEFT, DOWN, DOWN_RIGHT, RIGHT, UP_RIGHT, STOP };
+    
+    int value;
+    
+    private Direction(int value) {
+        this.value = value;
+    } 
+    
+    //TODO : 용도는???
+    public static Direction forDirection(int dirValue) {
+        for (Direction dir : DIRECTIONS_WITH_STOP)
+            if (dirValue == dir.value)
+                return dir;
+        return null;
+    }
+    
+    //TODO : 회전.. x값에 따라.. 변화되는 direction을 리턴하는데.. 정확한 원리는 추후에 확인필요
+    public static Direction rotate(Direction dir, int x) {
+        if (dir == STOP)
+            return STOP;
+        int ord;
+        for (ord = 0; ord < DIRECTIONS.length; ++ord)
+            if (dir == DIRECTIONS[ord])
+                break;
+        return DIRECTIONS[(ord + x) % DIRECTIONS.length];
+    }
 
+    public static Direction rotate(Direction dir) {
+        return rotate(dir, 1);
+    }
+    
+    //TODO : 랜덤은 언제 쓰는 걸까?
+    public static Direction randomDirection() {
+        return DIRECTIONS[new Random().nextInt(DIRECTIONS.length)];
+    }
+    
+    public static Direction randomDirectionIncludingStop() {
+        return DIRECTIONS_WITH_STOP[new Random().nextInt(DIRECTIONS_WITH_STOP.length)];
+    }
+    
+    /*
+     * dir.value의 값의 1비트 갯수가 2개이면 len은 1/루트2 의 몫
+     * dir.value가 UP.value
+     */
+    //TODO : 아래의 방향에 대한 로직은 추후 확인. 어렵다;;
+    public static double unitVectorX(Direction dir) {
+        double len = 1.0;
+        if (Integer.bitCount(dir.value) == 2)
+            len /= Math.sqrt(2.0);
+        if ((dir.value & LEFT.value) != 0)
+            return -len;
+        if ((dir.value & RIGHT.value) != 0)
+            return len;
+        return 0.0;
+    }
+    
+    public static double unitVectorY(Direction dir) {
+        double len = 1.0;
+        if (Integer.bitCount(dir.value) == 2)
+            len /= Math.sqrt(2.0);
+        if ((dir.value & UP.value) != 0)
+            return -len;
+        if ((dir.value & DOWN.value) != 0)
+            return len;
+        return 0.0;
+    }
+    
+    public static Direction compose(Direction base, Direction dir) {
+        int b = base.value, d = dir.value;
+        b |= d;
+        b &= ~(d << 2);
+        b &= ~(d >> 2);
+        return forDirection(b);
+    }
+    public static Direction erase(Direction base, Direction dir) {
+        int b = base.value, d = dir.value;
+        b &= ~d;
+        return forDirection(b);
+    }
+    
+    public static boolean includes(Direction base, Direction dir) {
+        return (base.value & dir.value) == dir.value;
+    }
+    public boolean includes(Direction dir) {
+        return (this.value & dir.value) == dir.value;
+    }
+    public int dimension() {
+        return Integer.bitCount(this.value);
+    }
 }
--- src/cjs/tankwar/component/tank/Tank.java
+++ src/cjs/tankwar/component/tank/Tank.java
@@ -1,6 +1,50 @@
 package cjs.tankwar.component.tank;
 
+import static java.awt.Color.black;
+import static java.awt.Color.gray;
+import static cjs.tankwar.component.Direction.STOP;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.Serializable;
+
+import cjs.tankwar.component.GameComponent;
+import cjs.tankwar.component.Direction;
+
+
 public class Tank extends GameComponent {
 
+	public static final int R = 18, HALF_WIDTH = 22, STEP = 4, CANNON_R = 5,
+            CANNON_LEN = 35;
+    public static final int SHOOT_CD = 70;
+    static final int EXPLOSION_R[] = { 15, 25, 30, 45, 35 };
+
+    public static final int DIALOG_BORDER_WIDTH = 5;
+    public static final int DIALOG_ARROW_WIDTH = 7;
+    public static final Color DIALOG_BACKGROUND_COLOR = new Color(255, 255,
+            255, 150);
+    public static final Color DIALOG_FOREGROUND_COLOR = black;
+    
+    //움직임방향, 슈팅방향 등으로 초기값 enum STOP으로 설정함
+    protected Direction moveDir = STOP;
+    protected Direction shootDir = STOP;
+    protected Direction cannonDir = STOP;
+    protected int power = 50;
+    protected int HP;
+    protected int maxHP;
+    public long blockTime = 0;
+    protected boolean ignoreMoveLimit = false;
+    protected int energyBarLastTime = 0;
+    protected Color clr1 = gray;
+    protected Color clr2 = black;
+    protected Color clr3 = null;
+    protected Dialog dialog = null;
+
+    protected Direction moveDirLimit = STOP;
+    protected int moveTimeLimit = 0;
+
 	
+    class Dialog implements Serializable {
+    	
+    }
 }
